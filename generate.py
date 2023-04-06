Increased numerical stability when training, removed ensemble sub-sampling due to bug.

generate.py
@@ -6,7 +6,7 @@ from os import mkdir
 
 import sys
 
-CUDA = False
+CUDA = torch.cuda.is_available() 
 
 #########################################################
 